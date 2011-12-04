f9af79c74646a18d425af690b99fd8fdb9146cf2
Robin Salkeld <robin.salkeld@gmail.com>
2011-12-03 22:23:59 -0800

    Another sample mirror implementation

---
+++ src/edu/ubc/mirrors/FieldMapMirror.java
@@ -0,0 +1,143 @@
+package edu.ubc.mirrors;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FieldMapMirror<T> implements ObjectMirror<T> {
+
+    private final Map<String, Object> fields;
+    private final Class<? extends T> klass;
+    
+    public FieldMapMirror(Class<? extends T> klass) {
+        this.klass = klass;
+        this.fields = new HashMap<String, Object>();
+    }
+    
+    @Override
+    public FieldMirror getMemberField(String name) throws NoSuchFieldException {
+        Field field = klass.getDeclaredField(name);
+        return new MapEntryFieldMirror(field);
+    }
+
+    @Override
+    public FieldMirror getStaticField(String name) throws NoSuchFieldException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Class<? extends T> getClassMirror() {
+        return klass;
+    }
+    
+    private class MapEntryFieldMirror implements FieldMirror {
+        private final Field field;
+
+        public MapEntryFieldMirror(Field field) {
+            this.field = field;
+        }
+        
+        @Override
+        public Class<?> getType() {
+            return field.getType();
+        }
+
+        @Override
+        public Object get() throws IllegalAccessException {
+            return fields.get(field.getName());
+        }
+
+        @Override
+        public boolean getBoolean() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? false : ((Boolean)value).booleanValue();
+        }
+
+        @Override
+        public byte getByte() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? 0 : ((Byte)value).byteValue();
+        }
+
+        @Override
+        public char getChar() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? 0 : ((Character)value).charValue();
+        }
+
+        @Override
+        public short getShort() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? 0 : ((Byte)value).byteValue();
+        }
+
+        @Override
+        public int getInt() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? 0 : ((Integer)value).intValue();
+        }
+
+        @Override
+        public long getLong() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? 0 : ((Long)value).longValue();
+        }
+
+        @Override
+        public float getFloat() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? 0 : ((Float)value).floatValue();
+        }
+
+        @Override
+        public double getDouble() throws IllegalAccessException {
+            Object value = get();
+            return value == null ? 0 : ((Double)value).doubleValue();
+        }
+
+        @Override
+        public void set(Object o) throws IllegalAccessException {
+            fields.put(field.getName(), o);
+        }
+
+        @Override
+        public void setBoolean(boolean b) throws IllegalAccessException {
+            set(Boolean.valueOf(b));
+        }
+
+        @Override
+        public void setByte(byte b) throws IllegalAccessException {
+            set(Byte.valueOf(b));
+        }
+
+        @Override
+        public void setChar(char c) throws IllegalAccessException {
+            set(Character.valueOf(c));
+        }
+
+        @Override
+        public void setShort(short s) throws IllegalAccessException {
+            set(Short.valueOf(s));
+        }
+
+        @Override
+        public void setInt(int i) throws IllegalAccessException {
+            set(Integer.valueOf(i));
+        }
+
+        @Override
+        public void setLong(long l) throws IllegalAccessException {
+            set(Long.valueOf(l));
+        }
+
+        @Override
+        public void setFloat(float f) throws IllegalAccessException {
+            set(Float.valueOf(f));
+        }
+
+        @Override
+        public void setDouble(double d) throws IllegalAccessException {
+            set(Double.valueOf(d));
+        }
+    }
+}
--- src/examples/MirageTest.java
+++ src/examples/MirageTest.java
@@ -1,5 +1,6 @@
 package examples;
 
+import edu.ubc.mirrors.FieldMapMirror;
 import edu.ubc.mirrors.NativeObjectMirror;
 import edu.ubc.mirrors.ObjectMirage;
 import edu.ubc.mirrors.ObjectMirror;
@@ -9,7 +10,7 @@ public class MirageTest {
     public static void main(String[] args) throws Exception {
         Bar bar = new Bar();
         Class<?> fooMirageClass = ObjectMirage.getMirageClass(Bar.class);
-        ObjectMirror<Bar> fooMirror = new NativeObjectMirror<Bar>(bar);
+        ObjectMirror<Bar> fooMirror = new FieldMapMirror<Bar>(Bar.class);
         Object barInstance = fooMirageClass.getConstructor(ObjectMirror.class).newInstance(fooMirror);
         fooMirageClass.getMethod("bar").invoke(barInstance);
     }
